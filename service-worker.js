6a0bcd6a365d0beae301ae1166c27e2997ef4c66
a01868293471-collab <a01868293471@gmail.com>
2026-09-04 06:40:27 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
 
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('a
+ctivate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return new Response("Network error occurred");
+    })
+  );
+});
